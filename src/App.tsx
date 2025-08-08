9ed714c8dd6f1b260cdd0711ee3730ee39eded46
NEXE-sudo <amishgandhi316@gmail.com>
2025-08-08 15:12:31 +0530

    Initial commit: upload existing codebase

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,14 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Courses from "./pages/Courses";
 import Rendezvous from "./pages/Rendezvous";
+import ResearchClub from "./pages/rendezvous/ResearchClub";
+import TechnologyClub from "./pages/rendezvous/TechnologyClub";
+import HindiClub from "./pages/rendezvous/HindiClub";
+import TheatreClub from "./pages/rendezvous/TheatreClub";
+import DebateClub from "./pages/rendezvous/DebateClub";
+import FinancialLiteracyClub from "./pages/rendezvous/FinancialLiteracyClub";
+import ArtCraftClub from "./pages/rendezvous/ArtCraftClub";
+import CommunityServiceDepartment from "./pages/rendezvous/CommunityServiceDepartment";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
@@ -32,6 +40,14 @@ const App = () => (
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/courses" element={<Courses />} />
                 <Route path="/rendezvous" element={<Rendezvous />} />
+                <Route path="/rendezvous/research" element={<ResearchClub />} />
+                <Route path="/rendezvous/tech" element={<TechnologyClub />} />
+                <Route path="/rendezvous/hindi" element={<HindiClub />} />
+                <Route path="/rendezvous/theatre" element={<TheatreClub />} />
+                <Route path="/rendezvous/debate" element={<DebateClub />} />
+                <Route path="/rendezvous/financial" element={<FinancialLiteracyClub />} />
+                <Route path="/rendezvous/art" element={<ArtCraftClub />} />
+                <Route path="/rendezvous/community" element={<CommunityServiceDepartment />} />
                 <Route path="/auth" element={<Auth />} />
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
